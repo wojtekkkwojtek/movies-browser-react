7a69228d87f51f009f613c1526290a605fcb5f44
Dorota Borowska <youcode2021db@gmail.com>
2022-04-07 14:27:47 +0200

    store, theme, GlobalStyle -files added

---
+++ src/core/App/theme.js
@@ -0,0 +1,14 @@
+export const theme = {
+    color: {
+        candleelight: "#FCD420",
+        white: "#FFFFFF",
+        mystic: "#E4E6F0",
+        whisper: "#F5F5FA",
+        stormGray: "#74788B",
+        waterloo: "#7E839A",
+        black: "#000000",
+    },
+    breakpoint: {
+        mobileMax: 767,
+    },
+};
