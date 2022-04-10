c3d43698c69640bbad42ff27d46dd20406b4db88
Dorota Borowska <youcode2021db@gmail.com>
2022-04-10 13:51:53 +0200

    loader animation built

--- src/common/Loader/styled.js
+++ src/common/Loader/styled.js
@@ -1,3 +1,25 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
+import { ReactComponent as Spinner } from "./spinner.svg";
 
-const Loader=styled.
+const loaderAnimation = keyframes`
+from{
+    transform:rotate(0deg);
+}
+to{
+    transform:rotate(360deg);
+}
+`;
+
+export const StyledSpinner = styled(Spinner)`
+    animation-name: ${loaderAnimation};
+    animation-duration: 1.8s;
+    animation-iteration-count: infinite;
+    animation-timing-function: linear;
+  `;
+
+export const Wrapper = styled.div`
+    display: flex;
+    justify-content: space-around;
+    margin-top:120px;
+  `;
+
