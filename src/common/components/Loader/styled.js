a321e27e2ee989653c80ef19de664a69c2b1df98
Dorota Borowska <youcode2021db@gmail.com>
2022-04-11 16:45:18 +0200

    heit in @media changed from 40px to 35px

--- src/common/components/Loader/styled.js
+++ src/common/components/Loader/styled.js
@@ -12,15 +12,14 @@ to{
 
 export const StyledSpinner = styled(Spinner)`
     animation: 1.8s infinite linear ${loaderAnimation};
-    
-    @media (max-width: ${({theme})=>theme.breakpoints.mobileMax}px){
-        height: 40px;
+
+    @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}) {
+        height: 35px;
     }
 `;
 
 export const Wrapper = styled.div`
     display: flex;
     justify-content: space-around;
-    margin-top:120px;
-  `;
-
+    margin-top: 120px;
+`;
