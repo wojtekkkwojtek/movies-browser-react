2cb59fb2a633fecc813c5a77667ba9892815b412
Dorota Borowska <youcode2021db@gmail.com>
2022-04-12 15:57:01 +0200

    general container created

--- src/core/App/App.js
+++ src/core/App/App.js
@@ -2,13 +2,16 @@ import { Loader } from "../../common/components/Loader";
 import Header from "../../common//components/Header";
 import Tile from "../../common/components/Tile";
 import Main from "../../common/Main/index";
+import { Container } from "../../common/components/Container";
 
 function App() {
   return (
     <div>
       <Header />
       {/* <Loader /> */}
-      <Tile />
+      <Container>
+        <Tile />
+      </Container>
       <Main />
     </div>
   );
