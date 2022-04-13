47cb8d83c248c3575375bc02a85a1fd6eb3dfd52
miloszzaj <zajaczkowski.milosz@gmail.com>
2022-04-13 16:32:26 +0200

    URLpopularMovies added to fetchedData

--- src/common/assets/generalData/fetchedData.js
+++ src/common/assets/generalData/fetchedData.js
@@ -1,4 +1,6 @@
 const API_KEY = process.env.REACT_APP_API_KEY
 
-export const URL = `https://api.themoviedb.org/4/list/1?api_key=${API_KEY}`
-console.log(API_KEY)
+export const URL = 'https://api.themoviedb.org/3'
+
+export const URLpopularMovies = `${URL}/movie/popular?api_key=${API_KEY}`
+console.log(URLpopularMovies)
