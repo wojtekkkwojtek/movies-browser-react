cc88ad057a8efba32d4908c7c39e9c40135ff8d7
miloszzaj <zajaczkowski.milosz@gmail.com>
2022-04-13 18:55:21 +0200

    movieList created functionality

--- src/common/assets/generalData/fetchedData.js
+++ src/common/assets/generalData/fetchedData.js
@@ -3,4 +3,5 @@ const API_KEY = process.env.REACT_APP_API_KEY
 export const URL = 'https://api.themoviedb.org/3'
 
 export const URLpopularMovies = `${URL}/movie/popular?api_key=${API_KEY}`
-console.log(URLpopularMovies)
+
+export const URLmovie = `${URL}/movie/popular?api_key=${API_KEY}`
