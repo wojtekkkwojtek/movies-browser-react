ba8977aaea27a8a3d068f8aad7d332620f54caab
miloszzaj <zajaczkowski.milosz@gmail.com>
2022-04-13 18:21:56 +0200

    movie page displayd in App

--- src/core/App/App.js
+++ src/core/App/App.js
@@ -1,20 +1,21 @@
-import { Loader } from "../../common/components/Loader";
-import Header from "../../common//components/Header";
-import Tile from "../../common/components/Tile";
-import Main from "../../common/Main/index";
-import { Container } from "../../common/components/Container";
+import { Loader } from '../../common/components/Loader'
+import Header from '../../common//components/Header'
+import Tile from '../../common/components/Tile'
+import Main from '../../common/Main/index'
+import { Container } from '../../common/components/Container'
+import MoviePage from '../../features/MoviePage'
 
 function App() {
-  return (
-    <div>
-      <Header />
-      {/* <Loader /> */}
-      <Container>
-        <Tile />
-      </Container>
-      <Main />
-    </div>
-  );
+    return (
+        <div>
+            <Header />
+            {/* <Loader /> */}
+            <Container>
+                <MoviePage />
+            </Container>
+            <Main />
+        </div>
+    )
 }
 
-export default App;
+export default App
