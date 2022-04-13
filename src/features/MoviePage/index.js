4a1e0dcabd82f5f9cc1c6af9b073312c191336f0
miloszzaj <zajaczkowski.milosz@gmail.com>
2022-04-13 18:20:36 +0200

    tile added to movie page

---
+++ src/features/MoviePage/index.js
@@ -0,0 +1,37 @@
+import { useEffect, useState } from 'react'
+import { URLpopularMovies } from '../../common/assets/generalData/fetchedData'
+import Tile from '../../common/components/Tile'
+import { Loader } from '../../common/components/Loader'
+
+const MoviePage = () => {
+    const [isLoading, setIsLoading] = useState(true)
+    const [movies, setMovies] = useState('')
+    useEffect(() => {
+        ;(async () => {
+            const response = await fetch(URLpopularMovies)
+            setMovies(await response.json())
+            console.log(movies)
+            setIsLoading(false)
+        })()
+    }, [movies])
+
+    // const title = data.results[0].original_title
+    return (
+        <>
+            {isLoading ? (
+                <Loader />
+            ) : (
+                movies.results.map((movie) => (
+                    <Tile
+                        title={movie.title}
+                        date={movie.release_date}
+                        rate={movie.vote_average}
+                        votes={movie.vote_count}
+                    />
+                ))
+            )}
+        </>
+    )
+}
+
+export default MoviePage
