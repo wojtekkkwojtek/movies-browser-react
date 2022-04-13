c7b11a4c2bb10e0b12e8afbbf75f051ac8b83a83
miloszzaj <zajaczkowski.milosz@gmail.com>
2022-04-13 18:55:40 +0200

    movieList functionallity added

---
+++ src/features/MovieList/index.js
@@ -0,0 +1,36 @@
+import { useEffect, useState } from 'react'
+import { URLpopularMovies } from '../../common/assets/generalData/fetchedData'
+import Tile from '../../common/components/Tile'
+import { Loader } from '../../common/components/Loader'
+
+const MovieList = () => {
+    const [isLoading, setIsLoading] = useState(true)
+    const [movies, setMovies] = useState('')
+    useEffect(() => {
+        ;(async () => {
+            const response = await fetch(URLpopularMovies)
+            setMovies(await response.json())
+            console.log(movies)
+            setIsLoading(false)
+        })()
+    }, [movies])
+
+    return (
+        <>
+            {isLoading ? (
+                <Loader />
+            ) : (
+                movies.results.map((movie) => (
+                    <Tile
+                        title={movie.title}
+                        date={movie.release_date.slice(0, 4)}
+                        rate={movie.vote_average}
+                        votes={movie.vote_count}
+                    />
+                ))
+            )}
+        </>
+    )
+}
+
+export default MovieList
