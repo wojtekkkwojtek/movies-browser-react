052e8359811e6fd648eaf087ff49a09a55eeac5f
miloszzaj <zajaczkowski.milosz@gmail.com>
2022-04-20 11:38:03 +0200

    HashRouter added to App.js

--- src/core/App/App.js
+++ src/core/App/App.js
@@ -1,21 +1,34 @@
+import { HashRouter, Link, Switch, Route } from 'react-router-dom'
+
 // import { Loader } from '../../common/components/Loader'
 import Header from '../../common//components/Header'
 import Main from '../../common/Main/index'
 import { Container } from '../../common/components/Container'
 import MoviePage from '../../features/MoviePage'
 import MovieList from '../../features/MovieList'
+import { toMovie, toMovies, toPeople } from './routes'
 
 function App() {
     return (
-        <div>
+        <HashRouter>
             <Header />
-            {/* <Loader /> */}
+            <Switch>
+                <Route>
+                    <MovieList path={toMovies} />
+                </Route>
+                <Route>
+                    <MoviePage path={toMovie} />
+                </Route>
+                <Route>{/* <PeopleList  path={toPeople} /> */}</Route>
+                <Route>{/* <PersonPage /> */}</Route>
+            </Switch>
+
             <Container>
                 <MovieList />
                 {/* <MoviePage /> */}
             </Container>
             <Main />
-        </div>
+        </HashRouter>
     )
 }
 
