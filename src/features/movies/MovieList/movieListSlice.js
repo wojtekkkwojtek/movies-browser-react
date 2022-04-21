1a97cb373c9e896a638e72763c2ae9a9673c7eb0
miloszzaj <zajaczkowski.milosz@gmail.com>
2022-04-21 15:30:33 +0200

    code refactoring in movieListSlice

--- src/features/movies/MovieList/movieListSlice.js
+++ src/features/movies/MovieList/movieListSlice.js
@@ -1,45 +1,55 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit'
 
 const movieListSlice = createSlice({
-    name: "movieList",
+    name: 'movies',
     initialState: {
         page: null,
         totalPages: null,
         totalResults: null,
         loading: false,
-        error: false,
+        error: 'cos',
         movieList: null,
     },
-    reducers:{
+    reducers: {
         fetchMovieList: (state) => {
-            state.loading = true;
-            state.error = false;
-            state.movieList = null;
+            state.loading = true
+            state.error = false
+            state.movieList = null
         },
-        fetchMovieListSuccess: (state, {payload}) => {
-            state.loading = false;
-            state.error = false;
-            state.movieList = payload.movieList;
-            state.totalPages = payload.totalPages;
-            state.totalResults = payload.totalResults;
+        fetchMovieListSuccess: (state, { payload }) => {
+            state.loading = false
+            state.error = false
+            state.movieList = payload.movieList
+            state.totalPages = payload.totalPages
+            state.totalResults = payload.totalResults
         },
         fetchMovieListError: (state) => {
-            state.loading = false;
-            state.error = true;
+            state.loading = false
+            state.error = true
+        },
+
+        fetchExample: () => {
+            console.log('test')
+        },
+        setMovieList: (state, { payload: example }) => {
+            state.movieList = example.results
         },
     },
-});
+})
 
 export const {
+    fetchExample,
+    setMovieList,
     fetchMovieList,
     fetchMovieListSuccess,
     fetchMovieListError,
-} = movieListSlice.actions;
+} = movieListSlice.actions
 
-export const selectMovieList = state => state.movieList;
-export const selectError = state => selectMovieList(state).error;
-export const selectLoading = state => selectMovieList(state).loading;
-export const selectTotalPages = state => selectMovieList(state).totalPages;
-export const selectTotalResults = state => selectMovieList(state).totalResults;
+export const selectMovieList = (state) => state.movies
+// export const selectError = (state) => selectMovieList(state)
+// export const selectLoading = (state) => selectMovieList(state).loading
+// export const selectTotalPages = (state) => selectMovieList(state).totalPages
+// export const selectTotalResults = (state) => selectMovieList(state).totalResults
 
-export const movieListReducer = movieListSlice.reducer;
+// export const getMovieById = (state, movieId) =>
+export const movieListReducer = movieListSlice.reducer
