7de04da2e69fc85745e7749d5d0d6366df1ed336
Dorota Borowska <youcode2021db@gmail.com>
2022-04-22 14:16:06 +0200

    peopleListReducer added

--- src/core/store.js
+++ src/core/store.js
@@ -4,6 +4,7 @@ import { watchFetchExample } from '../features/movies/MovieList/movieListSaga'
 import { genresReducer } from '../../src/common/assets/generalData/genresSlice'
 import { movieListReducer } from '../features/movies/MovieList/movieListSlice'
 import { moviePageReducer } from '../features/movies/MoviePage/moviePageSlice'
+import { peopleListReducer } from "../features/people/PeopleList/peopleListSlice"
 
 const sagaMiddleware = createSagaMiddleware()
 
@@ -12,6 +13,8 @@ const store = configureStore({
         genres: genresReducer,
         movies: movieListReducer,
         moviePage: moviePageReducer,
+        people: peopleListReducer,
+        // personPage: personPageReducer,
     },
     middleware: [sagaMiddleware],
 })
