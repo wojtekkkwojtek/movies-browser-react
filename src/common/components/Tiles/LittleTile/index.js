16d774ffff01a29d5a414081b4601c4b1785e07f
Dorota Borowska <youcode2021db@gmail.com>
2022-04-22 14:17:44 +0200

    'name' parameter added

--- src/common/components/Tiles/LittleTile/index.js
+++ src/common/components/Tiles/LittleTile/index.js
@@ -8,13 +8,14 @@ import {
 } from '../styled'
 import { URLimage } from '../../../assets/generalData/fetchedData'
 
-export const LittleTile = ({ poster, title }) => {
+export const LittleTile = ({ poster, title, name }) => {
     return (
         <TileContainer little>
             <Poster little src={`${URLimage}${poster}`} alt="" />
             <Content little>
                 <InfoWrapper little>
                     <InfoDetails>{title}</InfoDetails>
+                    <InfoDetails>{name}</InfoDetails>
                     <InfoDetails>Role</InfoDetails>
                 </InfoWrapper>
             </Content>
