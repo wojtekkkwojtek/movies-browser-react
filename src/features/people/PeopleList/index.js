999de84493a4765c67c116d50fb698898dfedbb1
Dorota Borowska <youcode2021db@gmail.com>
2022-04-25 19:51:42 +0200

    code cleaned

--- src/features/people/PeopleList/index.js
+++ src/features/people/PeopleList/index.js
@@ -1,14 +1,11 @@
-import { useEffect, useState } from 'react'
-import { URLpopularpeoples } from '../../../common/assets/generalData/fetchedData'
-import { Tile } from '../../../common/components/Tiles/BigTile'
-import { Loader } from '../../../common/components/Loader'
-import { nanoid } from 'nanoid'
-import { useNavigate } from 'react-router-dom'
-import { LittleTile } from '../../../common/components/Tiles/LittleTile'
-
-import { useParams } from 'react-router-dom'
-import { Container } from '../../../common/components/Container'
-import { useDispatch, useSelector } from 'react-redux'
+import { useEffect, useState } from 'react';
+import { LittleTile } from '../../../common/components/Tiles/LittleTile';
+import { Loader } from '../../../common/components/Loader';
+import { nanoid } from 'nanoid';
+import { useNavigate } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
+import { Container } from '../../../common/components/Container';
+import { useDispatch, useSelector } from 'react-redux';
 import {
     selectPeopleList,
     fetchExample,
@@ -16,24 +13,24 @@ import {
     fetchPeopleList,
     fetchPeopleListSuccess,
     fetchPeopleListError,
-} from './peopleListSlice'
+} from './peopleListSlice';
 
 const PeopleList = () => {
     // const isLoading = useSelector(selectLoading)
-    const { error, loading, peopleList } = useSelector(selectPeopleList)
-    console.log(error, loading, peopleList)
-    const dispatch = useDispatch()
+    const { error, loading, peopleList } = useSelector(selectPeopleList);
+    console.log(error, loading, peopleList);
+    const dispatch = useDispatch();
 
     const navigate = useNavigate()
     const routeChange = (id) => {
         navigate(`/people/${id}`)
     }
 
-    console.log(useParams())
+    console.log(useParams());
 
     useEffect(() => {
         dispatch(fetchExample())
-    }, [dispatch])
+    }, [dispatch]);
 
     return (
         <Container>
@@ -57,7 +54,7 @@ const PeopleList = () => {
             )}
         </Container>
     )
-}
+};
 
-export default PeopleList
+export default PeopleList;
 
