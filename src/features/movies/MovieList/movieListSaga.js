87fda684f5c323e60279389c742e3fd04e481011
Wojciech K <wojtekkk@protonmail.com>
2022-04-27 07:56:31 +0200

    yield call => yield put

--- src/features/movies/MovieList/movieListSaga.js
+++ src/features/movies/MovieList/movieListSaga.js
@@ -11,8 +11,7 @@ function* fetchMovieListHandler() {
         const fetchedMovies = yield call(getMovieList)
         yield put(setMovieList(fetchedMovies))
     } catch (error) {
-        yield call(fetchMovieListError())
-        yield call(alert, 'cos nie tak z listy')
+        yield put(fetchMovieListError());
     }
 }
 
