6112ff30f98c19f5fdbcbfef19e59c771483cc33
Dorota Borowska <youcode2021db@gmail.com>
2022-04-28 12:35:36 +0200

    import toPerson, PersonPage, fetchPersonPage

--- src/core/App/App.js
+++ src/core/App/App.js
@@ -1,14 +1,16 @@
 import { HashRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 // import { Loader } from '../../common/components/Loader'
 import Header from '../../common//components/Header'
 import MoviePage from '../../features/movies/MoviePage'
 import MovieList from '../../features/movies/MovieList'
 import PeopleList from '../../features/people/PeopleList'
-import { toMovie, toMovies, toPeople } from './routes'
+import PersonPage from '../../features/people/PersonPage'
+import { toMovie, toMovies, toPeople, toPerson } from './routes'
 import { fetchMovieList } from '../../features/movies/MovieList/movieListSlice'
-import { useDispatch } from 'react-redux'
 import { fetchPeopleList } from "../../features/people/PeopleList/peopleListSlice"
+import { fetchPersonPage } from "../../features/people/PersonPage/personPageSlice"
 
 // import { fetchMoviePage } from '../../features/movies/MoviePage/moviePageSlice'
 
@@ -19,6 +21,7 @@ function App() {
         const timer = setTimeout(() => {
             dispatch(fetchMovieList())
             dispatch(fetchPeopleList())
+            dispatch(fetchPersonPage()) //?
         }, 1000)
         return () => clearTimeout(timer)
     }, [])
@@ -29,12 +32,8 @@ function App() {
             <Routes>
                 <Route path={toMovies()} element={<MovieList />} />
                 <Route path={toMovie()} element={<MoviePage />} />
-                {/* <Route path={toMovie()}>
-                    <MoviePage />
-                </Route> */}
                 <Route path={toPeople()} element={<PeopleList />} />
-
-                <Route>{/* <PersonPage /> */}</Route>
+                <Route path={toPerson()} element={<PersonPage />} />
                 <Route path="/" element={<Navigate to="/movies" />} />
             </Routes>
         </HashRouter>
