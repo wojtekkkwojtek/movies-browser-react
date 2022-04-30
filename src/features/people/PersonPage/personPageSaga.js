a0037d668e37f8596863039b49f06ab4137ea5b4
Dorota Borowska <youcode2021db@gmail.com>
2022-04-30 13:54:22 +0200

    person credits got from Api

--- src/features/people/PersonPage/personPageSaga.js
+++ src/features/people/PersonPage/personPageSaga.js
@@ -1,20 +1,21 @@
-import { API_KEY, URL } from '../../../common/assets/generalData/fetchedData';
+import { API_KEY, URL, URLperson, URLpersonCredits } from '../../../common/assets/generalData/fetchedData';
 import { call, put, select, takeEvery } from 'redux-saga/effects';
 import { getPersonDetails } from '../../getApiData';
-import { fetchPersonPage, selectGetEx1, setPersonDetails } from './personPageSlice';
+import { fetchPersonPage, selectGetEx1, setPersonDetails, setPersonCredits } from './personPageSlice';
 
 console.log('selectGetEx1= ', selectGetEx1);
 
 function* fetchPersonDetailsHandler() {
     try {
         let personId = yield select(selectGetEx1);
-        const person = `${URL}/person/${personId}?api_key=${API_KEY}`;
-
-        console.log('person: ' + { person })       /////////////////
-        console.log('personId  ' + { personId })       /////////////
-        
-        const personDetails = yield call(getPersonDetails, person);
+        //  const person = `${URL}/person/${personId}?api_key=${API_KEY}`;
+        const personApiDetails = URLperson(personId);
+        const personApiCredits = URLpersonCredits(personId);
+        const personDetails = yield call(getPersonDetails, personApiDetails);
         yield put(setPersonDetails(personDetails));
+        const personCredits = yield call(getPersonDetails, personApiCredits);
+        yield put(setPersonCredits(personCredits));
+
     } catch (error) {
         yield call(alert, 'cos nie tak z person_details');
     }
