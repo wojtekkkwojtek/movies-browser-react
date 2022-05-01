7c704398d1a4871d6b2e2da6346d767924e30009
Dorota Borowska <youcode2021db@gmail.com>
2022-05-01 20:31:56 +0200

    URLperson, URLpersonCredit added

--- src/common/assets/generalData/fetchedData.js
+++ src/common/assets/generalData/fetchedData.js
@@ -1,5 +1,5 @@
-export const API_KEY = process.env.REACT_APP_API_KEY
-
+//export const API_KEY = process.env.REACT_APP_API_KEY
+export const API_KEY = "5a59313a8c2952c7e8707cf61f724f36";
 export const URL = 'https://api.themoviedb.org/3'
 
 export const URLpopularMovies = `${URL}/movie/popular?api_key=${API_KEY}`
@@ -9,6 +9,10 @@ export const URLpopularPeople = `${URL}/person/popular?api_key=${API_KEY}`
 export const URLmovie = `${URL}/movie/popular?api_key=${API_KEY}`
 
 export const URLpeople = `${URL}/person/popular?api_key=${API_KEY}`
+
+export const URLperson = personId => `${URL}person/${personId}/credits?api_key=${API_KEY}`
+
+export const URLpersonCredits = personId => `${URL}/${personId}/credits?api_key=${API_KEY}`
 
 export const URLimage = `https://image.tmdb.org/t/p/original`
 
