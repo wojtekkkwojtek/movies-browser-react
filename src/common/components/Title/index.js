0739bdd99b1495a9902e206018e884366b34f733
miloszzaj <zajaczkowski.milosz@gmail.com>
2022-05-01 19:14:16 +0200

    popular movies title styled for different devices

--- src/common/components/Title/index.js
+++ src/common/components/Title/index.js
@@ -7,4 +7,16 @@ export const Title = styled.h1`
     font-weight: 600;
     font-size: 36px;
     line-height: 120%;
+
+    @media (max-width: ${({ theme }) => theme.breakpoints.tablet}) {
+        text-align: center;
+        margin-top: 30px;
+    }
+
+    @media (max-width: ${({ theme }) => theme.breakpoints.mobileNormal}) {
+        text-align: left;
+        font-size: 18px;
+        margin-top: 24px;
+        margin-bottom: 0;
+    }
 `
