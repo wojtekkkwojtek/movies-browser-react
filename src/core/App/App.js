00bcc5ae850e936bd8157893034d93237624f544
Dorota Borowska <youcode2021db@gmail.com>
2022-05-02 17:37:15 +0200

    clearing code

--- src/core/App/App.js
+++ src/core/App/App.js
@@ -1,17 +1,11 @@
 import { HashRouter, Routes, Route, Navigate } from 'react-router-dom'
-import { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
-// import { Loader } from '../../common/components/Loader'
 import Header from '../../common//components/Header'
 import MoviePage from '../../features/movies/MoviePage'
 import MovieList from '../../features/movies/MovieList'
 import PeopleList from '../../features/people/PeopleList'
 import PersonPage from '../../features/people/PersonPage'
 import { toMovie, toMovies, toPeople, toPerson } from './routes'
-import { fetchMovieList } from '../../features/movies/MovieList/movieListSlice'
-import { useDispatch } from 'react-redux'
-import { fetchPeopleList } from '../../features/people/PeopleList/peopleListSlice'
-import { fetchPersonPage } from '../../features/people/PersonPage/personPageSlice'
+
 function App() {
     return (
         <HashRouter>
