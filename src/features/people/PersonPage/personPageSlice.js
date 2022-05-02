a1f63dbf1ff678ce0deb2766aa70d6acf784fba7
Dorota Borowska <youcode2021db@gmail.com>
2022-05-02 09:54:29 +0200

    payload: id removed

--- src/features/people/PersonPage/personPageSlice.js
+++ src/features/people/PersonPage/personPageSlice.js
@@ -1,30 +1,34 @@
 import { createSlice } from '@reduxjs/toolkit'
 
 const personPageSlice = createSlice({
-    name: 'personOne',
+    name: 'personPage',
     initialState: {
         personPage: [],
         loading: false,
         error: false,
     },
     reducers: {
-        fetchPersonPage: (state, { payload: id }) => {
+        fetchPersonPage: state => {
             state.loading = true
-            state.error = false
-            console.log(' fetchPersonPage ok?', 'id= ' + id)     ////////////////////////
-            state.person_id = id
+            state.error = false;
         },
 
-        setPersonDetails: (state, { payload: personDetails }) => {
+        fetchPersonPageSuccess: (state, { payload }) => {
             state.loading = false
-            state.personPage = personDetails
-            console.log('personPage:', personDetails)      ////////////////////////
+            state.error = false
+            state.personPage = payload
+            console.log('personPage_in_fetch_payload:', payload)      ////////////////////////
         },
+        // setPersonDetails: (state, { payload: personDetails }) => {
+        //     state.loading = false
+        //     state.personPage = personDetails
+        //     console.log('personPage set:', personDetails) ////////////////////////
+        // },
 
-        setPersonCredits: (state, { payload }) => {
-            state.cast = payload.cast;
-            state.crew = payload.crew;
-        }
+        // setPersonCredits: (state, { payload }) => {
+        //     state.cast = payload.cast;
+        //     state.crew = payload.crew;
+        // }
     },
 })
 
@@ -34,20 +38,15 @@ export const {
     fetchPersonPage,
     fetchPersonPageSuccess,
     fetchPersonPageError,
-
 } = personPageSlice.actions
 
-export const selectPersonPage = state => state.personOne
-export const selectPersonCast =state=> selectPersonPage(state).cast;
-export const selectPersonCrew =state=> selectPersonPage(state).crew;
+export const selectPersonPage = state => state.personPage
+export const selectPersonPageDetails=state=>selectPersonPage(state);
 
-
-export const selectGetEx1 = (state) => state.personOne.person_id
-console.log('selectGetEx1: ' + { selectGetEx1 })      ///////////////////////
-
-// export const selectLoading = (state) => selectMoviePage(state).loading
+//export const selectPersonCast = state => selectPersonDetails(state).cast;
+//export const selectPersonCrew = state => selectPersonDetails(state).crew;
+//export const selectLoading = (state) => selectPersonDetails(state).loading
 // export const selectError = (state) => selectMoviePage(state).error
-// export const selectActors = (state) => selectMoviePage(state).actors
-// export const selectMovie = (state) => selectMoviePage(state).moviePage
+
 
 export const personPageReducer = personPageSlice.reducer
