dab4f28e83cbac8ba8a69897b15dfeb19e80369b
Dorota Borowska <youcode2021db@gmail.com>
2022-05-02 17:39:14 +0200

    error section added

--- src/features/people/PersonPage/index.js
+++ src/features/people/PersonPage/index.js
@@ -4,44 +4,49 @@ import { nanoid } from 'nanoid'
 import { Tile } from '../../../common/components/Tiles/Tile'
 import { Loader } from '../../../common/components/Loader'
 import { Container } from '../../../common/components/Container'
-import { selectPersonPage, fetchPersonPage } from './personPageSlice'
+import { selectPersonPage, fetchPersonPage, selectError, selectLoading } from './personPageSlice'
 import { Title } from '../../../common/components/Title'
 import { useEffect } from "react"
+import { ErrorMessage } from '../../../common/components/ErrorMessage'
 
 const PersonPage = () => {
 
-    const personDetails = useSelector(selectPersonPage)
-    //const loading = useSelector(selectLoading)
-    const loading=true
+    const personPage  = useSelector(selectPersonPage)
+    const loading = useSelector(selectLoading)
+    const error = useSelector(selectError)
     const dispatch = useDispatch();
 
     const { id } = useParams();
-    console.log('id_from_useParams_in_PersonPage', id)
 
+    console.log('id__in_PersonPage', id) /////////////////////////
+    console.log('loading strony personPage:', loading) //////////////////////
 
     useEffect(() => {
         dispatch(fetchPersonPage(id));
     }, [id, dispatch])
 
+    console.log('personPage in index: ', personPage)
 
     return (
         <>
-            {loading ? (
-                <Loader />
-            ) : (
-                { personDetails } && (
+            {error && !loading && <ErrorMessage />}
+            {!error && loading && <Loader />}
+            {!error && !loading}
+            {!error && !loading && personPage && (
+                <>
                     <Container>
                         <Tile
                             key={nanoid()}
-                            name={personDetails.name}
-                            poster={personDetails.profile_path}
-                            country={personDetails.place_of_bird}
-                            overview={personDetails.biography}
+                            name={personPage.name}
+                            poster={personPage.profile_path}
+                            country={personPage.place_of_bird}
+                            overview={personPage.biography}
                         />
                         <Title>Cast</Title>
                         <Title>Crew</Title>
                     </Container>
-                )
+                </>
+
             )}
         </>
     )
