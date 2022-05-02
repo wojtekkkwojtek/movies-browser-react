ec6965c1911cf6dc40f62bc91004d2e77b883bca
Dorota Borowska <youcode2021db@gmail.com>
2022-05-02 22:47:09 +0200

    reducer's name changed into personPage

--- src/core/store.js
+++ src/core/store.js
@@ -13,7 +13,7 @@ const store = configureStore({
         movies: movieListReducer,
         movieOne: moviePageReducer,
         people: peopleListReducer,
-        personOne: personPageReducer,
+        personPage: personPageReducer,
     },
     middleware: [sagaMiddleware],
 })
