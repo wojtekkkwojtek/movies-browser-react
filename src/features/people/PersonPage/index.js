a7027f36652ab706dd7ed648c9f2bda04620dbca
Dorota Borowska <youcode2021db@gmail.com>
2022-05-02 09:55:37 +0200

    useEffect , useParams added

--- src/features/people/PersonPage/index.js
+++ src/features/people/PersonPage/index.js
@@ -1,28 +1,42 @@
-import { useSelector } from 'react-redux'
+import { useParams } from "react-router-dom"
+import { useSelector, useDispatch } from 'react-redux'
 import { nanoid } from 'nanoid'
 import { Tile } from '../../../common/components/Tiles/Tile'
 import { Loader } from '../../../common/components/Loader'
 import { Container } from '../../../common/components/Container'
-import { selectPersonPage } from './personPageSlice'
+import { selectPersonPage, fetchPersonPage } from './personPageSlice'
 import { Title } from '../../../common/components/Title'
+import { useEffect } from "react"
 
 const PersonPage = () => {
-    const { loading, personPage } = useSelector(selectPersonPage)
-console.log('personPage w index: '+{personPage})    //////////////////////////
+
+    const personDetails = useSelector(selectPersonPage)
+    //const loading = useSelector(selectLoading)
+    const loading=true
+    const dispatch = useDispatch();
+
+    const { id } = useParams();
+    console.log('id_from_useParams_in_PersonPage', id)
+
+
+    useEffect(() => {
+        dispatch(fetchPersonPage(id));
+    }, [id, dispatch])
+
+
     return (
         <>
             {loading ? (
                 <Loader />
             ) : (
-                { personPage } && (
-                    <Container >
-                        <Tile person
+                { personDetails } && (
+                    <Container>
+                        <Tile
                             key={nanoid()}
-                            title={personPage.name}
-                            poster={personPage.profile_path}
-                            country={personPage.place_of_bird}
-                            date={personPage.birthday}
-                            overview={personPage.biography}
+                            name={personDetails.name}
+                            poster={personDetails.profile_path}
+                            country={personDetails.place_of_bird}
+                            overview={personDetails.biography}
                         />
                         <Title>Cast</Title>
                         <Title>Crew</Title>
