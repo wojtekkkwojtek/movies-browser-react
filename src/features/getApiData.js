9c5affcc9f10bf32566f8cb7cc0b63af3087e32d
Dorota Borowska <youcode2021db@gmail.com>
2022-05-04 17:56:04 +0200

    code formatted and cleaned

--- src/features/getApiData.js
+++ src/features/getApiData.js
@@ -5,7 +5,7 @@ import {
     URLpopularMovies,
     URLmovieDetails,
     URLpopularPeople,
-     URLpersonDetails,
+    URLpersonDetails,
     URL
 } from '../common/assets/generalData/fetchedData'
 
@@ -42,42 +42,7 @@ export const getPeopleList = async () => {
     return await response.json()
 }
 
-// export const getMovieDetails = async () => {
-//     const response = await fetch(URLmovieDetails)
 
-//     if (!response.ok) {
-//         new Error(response.statusText)
-//     }
-
-//     return await response.json()
-// }
-// useEffect(() => {
-//     const getApi = () => {
-//         ;(async (URL) => {
-//             try {
-//                 const response = await axios.get(URL)
-//                 console.log('response:', response)
-//                 return response
-//             } catch (error) {
-//                 console.log('coś nie tak', error)
-//             }
-//         })()
-//     }
-//     console.log(getApi())
-// }, [])
-// export default getApi
-
-// export const getApi = (URL) => {
-//     ;(async (URL) => {
-//         try {
-//             const response = await axios.get(URL)
-//             console.log('response:', response)
-//             return response
-//         } catch (error) {
-//             console.log('coś nie tak', error)
-//         }
-//     })()
-// }
 
 
 export const getMovieDetails = async (URL) => {
