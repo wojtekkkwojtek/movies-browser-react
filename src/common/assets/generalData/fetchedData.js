ef63859452be5e8fac0e3a5ee1634de2d92d6ce2
Dorota Borowska <youcode2021db@gmail.com>
2022-05-04 15:34:45 +0200

    URLpersonDetails changed

--- src/common/assets/generalData/fetchedData.js
+++ src/common/assets/generalData/fetchedData.js
@@ -1,5 +1,5 @@
-//export const API_KEY = process.env.REACT_APP_API_KEY
-export const API_KEY="5a59313a8c2952c7e8707cf61f724f36";
+export const API_KEY = process.env.REACT_APP_API_KEY
+
 export const URL = 'https://api.themoviedb.org/3'
 
 export const URLgenres = `${URL}/genre/movie/list?api_key=${API_KEY}`
@@ -12,9 +12,9 @@ export const URLmovie = `${URL}/movie/popular?api_key=${API_KEY}`
 
 export const URLpeople = `${URL}/person/popular?api_key=${API_KEY}`
 
-export const URLpersonDetails = personId => `${URL}person/${personId}?api_key=${API_KEY}`
+export const URLpersonDetails = personId => `${URL}/person/${personId}?api_key=${API_KEY}`
 
-export const URLpersonCredits = personId => `${URL}/${personId}/credits?api_key=${API_KEY}`
+export const URLpersonCredits = personId => `${URL}/person/${personId}/credits?api_key=${API_KEY}`
 
 export const URLimage = `https://image.tmdb.org/t/p/original`
 
