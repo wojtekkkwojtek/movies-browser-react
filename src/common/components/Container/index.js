4b38c402d899025bb5404ba028eba1b16dee8fe7
Dorota Borowska <youcode2021db@gmail.com>
2022-05-07 23:36:19 +0200

    justify-content changed to flex-start

--- src/common/components/Container/index.js
+++ src/common/components/Container/index.js
@@ -4,10 +4,11 @@ export const Container = styled.div`
     max-width: 1368px;
     margin: auto;
     display: flex;
-    justify-content: center;
+    justify-content: flex-start;
 
     flex-wrap: wrap;
     gap: 24px;
+    padding-bottom: 16px;
 
     @media (max-width: ${({ theme }) => theme.breakpoints.mobileNormal}) {
         padding: 0 16px;
