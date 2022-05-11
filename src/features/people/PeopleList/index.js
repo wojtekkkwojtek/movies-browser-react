ab8e6fd5222a13339647a6e8e6e4fe532726896f
miloszzaj <zajaczkowski.milosz@gmail.com>
2022-05-11 23:46:07 +0200

    useSearch added in peopleList

--- src/features/people/PeopleList/index.js
+++ src/features/people/PeopleList/index.js
@@ -5,38 +5,41 @@ import { PersonTile } from '../../../common/components/Tiles/PersonTile'
 import { Title } from '../../../common/components/Title'
 import { Loader } from '../../../common/components/Loader'
 import { nanoid } from 'nanoid'
-import { useNavigate } from 'react-router-dom'
-import { useParams } from 'react-router-dom'
+import { useLocation, useNavigate } from 'react-router-dom'
 import { Container } from '../../../common/components/Container'
 import { selectPeopleList, fetchPeopleList } from './peopleListSlice'
 import { ErrorMessage } from '../../../common/components/ErrorMessage'
 import { Pagination } from '../../../common/components/Pagination'
+import { useSearch } from '../../useSearch'
 
 const PeopleList = () => {
     const { loading, peopleList, error } = useSelector(selectPeopleList)
-
     const dispatch = useDispatch()
+    const navigate = useNavigate()
+    const location = useLocation()
+
+    const query = useSearch('search', location)
     useEffect(() => {
         dispatch(fetchPeopleList())
     }, [dispatch])
 
-    const navigate = useNavigate()
-
     const routeChange = (id) => {
         navigate(`/people/${id}`)
     }
+    const showTitle = () => (query ? `Search for "${query}"` : 'Popular movies')
 
     return (
         <>
             <Container>
-                {error ? (
-                    <ErrorMessage />
-                ) : loading ? (
+                {error && !loading && <ErrorMessage />}
+                {!error && loading && (
                     <>
-                        <Title title="Popular people" />
-                        <Loader />
+                        <Title title={showTitle()} /> <Loader />
                     </>
-                ) : (
+                )}
+                {!error && !loading && <Title title={showTitle()} />}
+                {!error &&
+                    !loading &&
                     peopleList &&
                     peopleList.map((people) => (
                         <>
@@ -47,8 +50,7 @@ const PeopleList = () => {
                                 poster={people.profile_path}
                             />
                         </>
-                    ))
-                )}
+                    ))}
             </Container>
             {!error && !loading && <Pagination />}
         </>
