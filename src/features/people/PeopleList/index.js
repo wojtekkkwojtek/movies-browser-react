12d7745a638150bebd763481e22253cfaffc9d8d
Dorota Borowska <youcode2021db@gmail.com>
2022-05-20 00:45:17 +0200

    no pagination in case of one page only

--- src/features/people/PeopleList/index.js
+++ src/features/people/PeopleList/index.js
@@ -6,7 +6,7 @@ import { Title } from '../../../common/components/Title'
 import { Loader } from '../../../common/components/Loader'
 import { useLocation, useNavigate } from 'react-router-dom'
 import { Section } from '../../../common/components/Section'
-import { selectPeopleList, fetchPeopleList, selectTotalPeoplelResults } from './peopleListSlice'
+import { selectPeopleList, fetchPeopleList, selectTotalPeoplelResults, selectTotalPeoplePages } from './peopleListSlice'
 import { ErrorMessage } from '../../../common/components/ErrorMessage'
 import { Pagination } from '../../../common/components/Pagination'
 import { useSearch } from '../../useParameters'
@@ -21,10 +21,12 @@ const PeopleList = () => {
     const totalPeopleResults = useSelector(selectTotalPeoplelResults);
     const query = useSearch(queryKeys.search, location)
     const page = useSearch(queryKeys.page, location)
+    const totalPeoplePages = useSelector(selectTotalPeoplePages)
 
+    console.log('totalPeoplePages', totalPeoplePages)
     console.log('totalPeopleResults', totalPeopleResults)
     console.log('query', query)
-
+    console.log('peopleList', { peopleList })
     useEffect(() => {
         dispatch(fetchPeopleList({ query, page }))
     }, [dispatch, query, page])
@@ -38,13 +40,13 @@ const PeopleList = () => {
             <Section>
                 {query && !error && !loading && (totalPeopleResults === 0) ?
                     <>
-                        <Title title={`Sorry, no result for "${query[0].toUpperCase()+query.slice(1)}"(${totalPeopleResults})"`} />
+                        <Title title={`Sorry, no result for "${query[0].toUpperCase() + query.slice(1)}"(${totalPeopleResults})"`} />
                         <NoResultMessage />
                     </>
                     :
                     <Title title=
                         {query
-                            ? `Search results for "${query[0].toUpperCase()+query.slice(1)}"(${totalPeopleResults})`
+                            ? `Search results for "${query[0].toUpperCase() + query.slice(1)}"(${totalPeopleResults})`
                             : `Popular people`}
                     />
                 }
@@ -65,8 +67,8 @@ const PeopleList = () => {
                     ))
                 }
             </Section>
-            {/* {error && loading && !totalPeopleResults && <NoResultMessage />} */}
-            {!error && !loading && totalPeopleResults && <Pagination />}
+            {/* {!error && !loading && totalPeopleResults && <Pagination />} */}
+            {!error && !loading && totalPeopleResults && totalPeoplePages > 1 && <Pagination />}
         </>
     )
 }
